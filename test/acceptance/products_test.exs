3ba19306dd876f9e32fd64d6441a6f9b459d0ecc
Josh Adams <josh.rubyist@gmail.com>
2016-01-13 11:32:36 -0600

    Add link to product show

--- test/acceptance/products_test.exs
+++ test/acceptance/products_test.exs
@@ -5,19 +5,23 @@ defmodule PhoenixCommerce.Acceptance.ProductsTest do
   alias PhoenixCommerce.Product
   alias PhoenixCommerce.Repo
 
-  test "/products has a list of products" do
-    navigate_to "/products"
-    assert find_element(:css, "ul.products")
-  end
-
-  test "/products - product details include name, description, and price" do
+  setup do
+    Repo.delete_all(Product)
     {:ok, product} =
       %Product{
         name: "Some product",
         description: "Some product description",
         price: Decimal.new("25.20")
       } |> Repo.insert
+    {:ok, product: product}
+  end
 
+  test "/products has a list of products" do
+    navigate_to "/products"
+    assert find_element(:css, "ul.products")
+  end
+
+  test "/products - product details include name, description, and price", %{product: product} do
     navigate_to "/products"
     product_li = find_element(:css, "ul.products li")
     name = find_within_element(product_li, :css, "h3")
@@ -26,5 +30,13 @@ defmodule PhoenixCommerce.Acceptance.ProductsTest do
     assert visible_text(name) == product.name
     assert visible_text(price) == "$#{product.price}"
     assert visible_text(description) == product.description
+  end
+
+  test "/products - click a product to view its details", %{product: product} do
+    navigate_to "/products"
+    product_li = find_element(:css, "ul.products li")
+    link = find_within_element(product_li, :css, "a")
+    click(link)
+    assert "/products/#{product.id}" == current_path
   end
 end
